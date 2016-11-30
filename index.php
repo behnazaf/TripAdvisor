7ae92d8e69b8b127868b5607d589e0d6902e2b81
Behnaz <behnazakbaria@yahoo.com>
2016-11-29 23:46:37 -0500

    ----Behnaz--------     first create

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+$app = new \Slim\Slim(array(
+    'view' => new \Slim\Views\Twig()
+        ));
+
+$view = $app->view();
+$view->parserOptions = array(
+    'debug' => true,
+    'cache' => dirname(__FILE__) . '/cache'
+);
+$view->setTemplatesDirectory(dirname(__FILE__) . '/templates');
+
+
+
+
